608142a12bc030f24ff839c98a7d5690f39537ea
maitri0204 <maitripatel2608@gmail.com>
2024-06-12 19:35:21 +0530

    Changes to be committed: 	new file:   Data Science/Introduction to Data Visualization with Seaborn.ipynb 	new file:   Python/Week2/Deletingvariable.py 	new file:   Python/Week2/EscapeCharacters.py 	new file:   Python/Week2/Strings.py

---
+++ Python/Week2/Deletingvariable.py
@@ -0,0 +1,5 @@
+# delete a particular variable
+a=5
+print(a)
+del a
+print(a)
---
+++ Python/Week2/EscapeCharacters.py
@@ -0,0 +1,11 @@
+print('It\'s a beautiful day')
+print("We are from \"IIT\" Madras")
+
+x='This is a string'
+y='This is also a string'
+z='''first string
+second string
+third string'''
+print(x)
+print(y)
+print(z)
